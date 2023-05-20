db0bd94533259baa4c462312ed24eafde25c12a2
Pozsa Marcell (SZF_E_2022_2024) <pozsa.marcell@diak.szamalk-szalezi.hu>
2023-05-20 12:52:27 +0200

    teszt esetek befejezve

--- src/fizetesprojekt/FizetesProjekt.java
+++ src/fizetesprojekt/FizetesProjekt.java
@@ -20,7 +20,7 @@ public class FizetesProjekt {
     }
 
     public FizetesProjekt() throws IOException {
-        sorok = Files.readAllLines(Path.of("szem_adatok.txt"));
+        sorok = Files.readAllLines(Path.of("szem_adatok2.txt"));
         szemAdat = new SzemelyesAdat[sorok.size() - 1];
         for (int i = 1; i < sorok.size(); i++) {
 
@@ -56,7 +56,7 @@ public class FizetesProjekt {
     }
 
     private void feladat1() {
-
+        System.out.println("1.feladat");
         System.out.println("Ki keresi a legtöbbet?:");
         int index = 0;
         for (int i = 1; i < szemAdat.length; i++) {
@@ -66,10 +66,13 @@ public class FizetesProjekt {
 
         }
         System.out.println(szemAdat[index].getNev() + " keresi a legtöbbet");
+        String nev = szemAdat[index].getNev();
+        assert nev.equals("Xénia") : "1. hibás válasz";
 
     }
 
     private void feladat2() {
+        System.out.println("2.feladat");
         System.out.println("mennyi az átlagfizetés?");
         int sum = 0, osszSzam = szemAdat.length;
         for (int i = 0; i < szemAdat.length; i++) {
@@ -77,9 +80,11 @@ public class FizetesProjekt {
         }
         double atlag = sum / osszSzam;
         System.out.println(atlag + " Ft");
+        assert atlag == 501_800: "2. hibás válasz";
     }
 
     private void feladat3() {
+        System.out.println("3.feladat");
         System.out.println("mindenki budapesti?");
         boolean budapestMind;
         int i = 0, N = szemAdat.length;
@@ -96,38 +101,49 @@ public class FizetesProjekt {
         }
         budapestMind = i > N;
         System.out.println(budapestMind + " index: " + i + " " + szemAdat[i].getCim());
+        
+        assert budapestMind == false: "3. hibás válasz";
+        assert i <= 1 : "3. hibás progTétel"; 
 
     }
 
     private void feladat4() {
+        System.out.println("4.feladat");
         int i = 0, N = szemAdat.length;
         boolean vane20felettiBP;
         int index = 0;
         System.out.println("van e 20 év feletti budapesti?");
 
-        while (i < N && !(szemAdat[i].getCim().equals("Budapest")) && szemAdat[i].getKor() >= 20) {
-
+        while (i < N && !(szemAdat[i].getCim().equals("Bp")) && szemAdat[i].getKor() >= 20) {
+          
             i++;
         }
-        vane20felettiBP = i > 0;
+        vane20felettiBP = i < N;
         if (vane20felettiBP) {
-            System.out.println(vane20felettiBP + " index: " + i + " " + szemAdat[i].getKor() + "éves");
+            System.out.println(vane20felettiBP + " index: " +  i +" " + szemAdat[i].getKor() + "éves");
         } else {
             System.out.println("nincs");
         }
+        
+        assert vane20felettiBP == true: "4. hibás válasz";
+        assert i == 0 : "4. hibás progTétel";
     }
 
     private void feladat5() {
+        System.out.println("5.feladat");
         System.out.println("Milyen címek vannak eltárolva?:");
         HashSet<String> varosok = new HashSet<>();
         for (SzemelyesAdat adat : szemAdat) {
             varosok.add(adat.getCim());
         }
         System.out.println(varosok);
+        assert varosok.size() == 1 : "5. hibás méret";
+	assert varosok.contains("Bp") : "5. hibás adat";
 
     }
 
     private void feladat6() {
+        System.out.println("6.feladat");
         System.out.println("melyik címen hányan laknak? ");
         Map<String, Integer> varosokDB = new HashMap<>();
         for (SzemelyesAdat adat : szemAdat) {
@@ -146,6 +162,9 @@ public class FizetesProjekt {
             Integer value = entry.getValue();
             System.out.printf("%-11s: %4d db \n", key, value);
         }
+        assert varosokDB.size() == 1 : "6. hibás méret";
+	assert varosokDB.containsKey("Bp") : "6. hibás kulcs";
+	assert varosokDB.get("Bp") == 5 : "6. hibás érték";
     }
 
     private void Linebreak() {
@@ -153,12 +172,13 @@ public class FizetesProjekt {
     }
 
     private void feladat7() throws IOException {
+        System.out.println("7.feladat");
         System.out.println("nem budapestiek minden adata a nemBp.txt");
         List<SzemelyesAdat> nemBPlakosok = new ArrayList<>();
         List<SzemelyesAdat> BPlakosok = new ArrayList<>();
         int i = 0;
         for (SzemelyesAdat adat : szemAdat) {
-            if (!(adat.getCim().equals("Budapest"))) {
+            if (!(adat.getCim().equals("Bp"))) {
                 nemBPlakosok.add(adat);
             }
         }
